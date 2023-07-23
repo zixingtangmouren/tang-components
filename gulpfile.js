582e8cb190ddaec0260434159cb0231ad57ca7d5
tangmouren <1678977488@qq.com>
2023-07-23 21:04:23 +0800

    build: 完善构建工程

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const del = require('del');
 const babel = require('gulp-babel');
 const webpack = require('webpack');
 const path = require('path');
+const less = require('gulp-less');
 
 function clean() {
   return del('lib/**');
@@ -16,7 +17,11 @@ function buildES() {
     module: 'ES6',
   });
 
-  return gulp.src('src/**/*.{ts,tsx}').pipe(tsCompiler).pipe(gulp.dest('lib/es/'));
+  return gulp
+    .src('src/**/*.{ts,tsx}')
+    .pipe(tsCompiler)
+    .pipe(babel({ plugins: ['./babel-transform-less-to-css'] }))
+    .pipe(gulp.dest('lib/es/'));
 }
 
 function buildCJS() {
@@ -41,14 +46,61 @@ function buildDecalration() {
   return gulp.src('src/**/*.{ts,tsx}').pipe(tsCompiler).pipe(gulp.dest('lib/es/')).pipe(gulp.dest('lib/cjs/'));
 }
 
-function buildUMD() {
-  webpack({
+function buildStyles() {
+  return gulp.src(['src/**/*.less']).pipe(less()).pipe(gulp.dest('lib/es/')).pipe(gulp.dest('lib/cjs/'));
+}
+
+function buildUMD(cb) {
+  const compiler = webpack({
     entry: path.resolve(__dirname, 'lib/es/index.js'),
     output: {
       filename: 'tang-components.js',
-      library: {},
+      library: {
+        name: 'tangComponents',
+        type: 'umd',
+      },
+      path: path.resolve(__dirname, 'lib/umd'),
     },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [['@babel/preset-env', {}], '@babel/preset-react'],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.json'],
+    },
+    externals: [
+      {
+        react: {
+          commonjs: 'react',
+          commonjs2: 'react',
+          amd: 'react',
+          root: 'React',
+        },
+      },
+    ],
+  });
+
+  compiler.run();
+
+  compiler.hooks.done.tap('buildUMDTask', () => {
+    cb();
   });
 }
 
-exports.default = gulp.series(clean, buildES, buildCJS, buildDecalration);
+exports.default = gulp.series(clean, buildES, buildCJS, gulp.parallel(buildStyles, buildDecalration), buildUMD);
